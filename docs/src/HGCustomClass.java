98d777ea31913674fe63175f107d5b9e2efb21c4
Dmitry Ponyatov <dponyatov@gmail.com>
2017-05-27 15:14:30 +0400

    + hgdb custom user class
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

---
+++ docs/src/HGCustomClass.java
@@ -0,0 +1,12 @@
+public class HGCustomClass {
+	
+	String property;			// property
+	public HGCustomClass() {}	// null-constructor
+
+// getter/setter implementation
+
+	public String getProperty() {
+		return property; }
+	public void setProperty(String property) {
+		this.property = property; }
+}
